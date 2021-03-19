fd6e6ca69cd87be88b55a72b6a1b3c26af5b70ce
Benjamin Hoover <benjamin.hoover@ibm.com>
2021-03-19 15:43:03 -0400

    Use Tailwindcss/jit

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,12 @@
 const sveltePreprocess = require('svelte-preprocess');
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./src/**/*.svelte', "assets/style.css"],
-  whitelistPatterns: [/svelte-/],
-  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-});
-const production = !process.env.ROLLUP_WATCH;
-
 
 module.exports = {
   preprocess: [
     sveltePreprocess({
       postcss: {
         plugins: [
-          require('tailwindcss'),
+          require('@tailwindcss/jit'),
           require('autoprefixer'),
-          ...(production ? [purgecss] : []) 
         ],
       },
     }),
