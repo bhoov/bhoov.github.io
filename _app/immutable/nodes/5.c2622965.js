6446b090c9c7be56de3101dd98343945ed66c646
Benjamin Hoover <benhoover34@gmail.com>
2026-06-09 13:57:08 -0400

    Updates

---
+++ _app/immutable/nodes/5.c2622965.js
@@ -0,0 +1,29 @@
+import{S as he,i as de,s as fe,k as s,q as c,a as w,l as n,m as i,r as h,h as r,c as b,n as t,b as ue,F as e,J as V}from"../chunks/index.6a9e29a0.js";function me(H){let a,p,_,k,y,L,d,j,O,R,E,W,q,M,B,C,A,F,U,l,D,f,T,X,I,u,J,z,P,m,G,K,x,v,Q;return{c(){a=s("main"),p=s("p"),_=c("WIP: barebones homepage for this work"),k=w(),y=s("p"),L=c(`Check this out in person! If you will be at NeurIPS'23, let's connect at the
+    AMHN (Associative Memory & Hopfield Network) Workshop! Fri Dec 15 Room 223.
+    Details at `),d=s("a"),j=c("NeurIPS 2023 virtual page"),O=c("."),R=w(),E=s("h1"),W=c(`Memory in Plain Sight: A Survey of the Uncanny Resemblances between
+    Diffusion Models and Associative Memories`),q=w(),M=s("p"),B=c(`Diffusion Models (DMs) have recently set state-of-the-art on many generation
+    benchmarks. However, there are myriad ways to describe them mathematically,
+    which makes it difficult to develop a simple understanding of how they work.
+    In this submission, we provide a concise overview of DMs from the
+    perspective of dynamical systems and Ordinary Differential Equations (ODEs)
+    which exposes a mathematical connection to the highly related yet often
+    overlooked class of energy-based models, called Associative Memories (AMs).
+    Energy-based AMs are a theoretical framework that behave much like denoising
+    DMs, but they enable us to directly compute a Lyapunov energy function on
+    which we can perform gradient descent to denoise data. We finally identify
+    the similarities and differences between AMs and DMs, discussing new
+    research directions revealed by the extent of their similarities.`),C=w(),A=s("h2"),F=c("Links"),U=w(),l=s("ul"),D=s("li"),f=s("a"),T=c("OpenReview (short version)"),X=w(),I=s("li"),u=s("a"),J=c("Initial Twitter Announcement"),z=w(),P=s("li"),m=s("a"),G=c("Paper on ArXiv (long version)"),K=w(),x=s("li"),v=s("a"),Q=c("NeurIPS 2023 virtual page"),this.h()},l(S){a=n(S,"MAIN",{});var o=i(a);p=n(o,"P",{class:!0});var Y=i(p);_=h(Y,"WIP: barebones homepage for this work"),Y.forEach(r),k=b(o),y=n(o,"P",{class:!0});var N=i(y);L=h(N,`Check this out in person! If you will be at NeurIPS'23, let's connect at the
+    AMHN (Associative Memory & Hopfield Network) Workshop! Fri Dec 15 Room 223.
+    Details at `),d=n(N,"A",{href:!0,target:!0,rel:!0,class:!0});var Z=i(d);j=h(Z,"NeurIPS 2023 virtual page"),Z.forEach(r),O=h(N,"."),N.forEach(r),R=b(o),E=n(o,"H1",{});var $=i(E);W=h($,`Memory in Plain Sight: A Survey of the Uncanny Resemblances between
+    Diffusion Models and Associative Memories`),$.forEach(r),q=b(o),M=n(o,"P",{class:!0});var ee=i(M);B=h(ee,`Diffusion Models (DMs) have recently set state-of-the-art on many generation
+    benchmarks. However, there are myriad ways to describe them mathematically,
+    which makes it difficult to develop a simple understanding of how they work.
+    In this submission, we provide a concise overview of DMs from the
+    perspective of dynamical systems and Ordinary Differential Equations (ODEs)
+    which exposes a mathematical connection to the highly related yet often
+    overlooked class of energy-based models, called Associative Memories (AMs).
+    Energy-based AMs are a theoretical framework that behave much like denoising
+    DMs, but they enable us to directly compute a Lyapunov energy function on
+    which we can perform gradient descent to denoise data. We finally identify
+    the similarities and differences between AMs and DMs, discussing new
+    research directions revealed by the extent of their similarities.`),ee.forEach(r),C=b(o),A=n(o,"H2",{class:!0});var te=i(A);F=h(te,"Links"),te.forEach(r),U=b(o),l=n(o,"UL",{});var g=i(l);D=n(g,"LI",{});var ae=i(D);f=n(ae,"A",{href:!0,target:!0,rel:!0,class:!0});var re=i(f);T=h(re,"OpenReview (short version)"),re.forEach(r),ae.forEach(r),X=b(g),I=n(g,"LI",{});var se=i(I);u=n(se,"A",{href:!0,target:!0,rel:!0,class:!0});var ne=i(u);J=h(ne,"Initial Twitter Announcement"),ne.forEach(r),se.forEach(r),z=b(g),P=n(g,"LI",{});var ie=i(P);m=n(ie,"A",{href:!0,target:!0,rel:!0,class:!0});var oe=i(m);G=h(oe,"Paper on ArXiv (long version)"),oe.forEach(r),ie.forEach(r),K=b(g),x=n(g,"LI",{});var le=i(x);v=n(le,"A",{href:!0,target:!0,rel:!0,class:!0});var ce=i(v);Q=h(ce,"NeurIPS 2023 virtual page"),ce.forEach(r),le.forEach(r),g.forEach(r),o.forEach(r),this.h()},h(){t(p,"class","text-gray-500 text-sm italic my-8"),t(d,"href","https://neurips.cc/virtual/2023/workshop/66524"),t(d,"target","_blank"),t(d,"rel","noreferrer"),t(d,"class","svelte-1jp7cc4"),t(y,"class","text-xl text-gray-500"),t(M,"class","abstract"),t(A,"class","svelte-1jp7cc4"),t(f,"href","https://openreview.net/forum?id=B1BL9go65H"),t(f,"target","_blank"),t(f,"rel","noreferrer"),t(f,"class","svelte-1jp7cc4"),t(u,"href","https://twitter.com/Ben_Hoov/status/1708912750507024401"),t(u,"target","_blank"),t(u,"rel","noreferrer"),t(u,"class","svelte-1jp7cc4"),t(m,"href","https://arxiv.org/abs/2309.16750"),t(m,"target","_blank"),t(m,"rel","noreferrer"),t(m,"class","svelte-1jp7cc4"),t(v,"href","https://neurips.cc/virtual/2023/workshop/66524"),t(v,"target","_blank"),t(v,"rel","noreferrer"),t(v,"class","svelte-1jp7cc4")},m(S,o){ue(S,a,o),e(a,p),e(p,_),e(a,k),e(a,y),e(y,L),e(y,d),e(d,j),e(y,O),e(a,R),e(a,E),e(E,W),e(a,q),e(a,M),e(M,B),e(a,C),e(a,A),e(A,F),e(a,U),e(a,l),e(l,D),e(D,f),e(f,T),e(l,X),e(l,I),e(I,u),e(u,J),e(l,z),e(l,P),e(P,m),e(m,G),e(l,K),e(l,x),e(x,v),e(v,Q)},p:V,i:V,o:V,d(S){S&&r(a)}}}function ve(H,a,p){let{data:_}=a;return H.$$set=k=>{"data"in k&&p(0,_=k.data)},[_]}class ye extends he{constructor(a){super(),de(this,a,ve,me,fe,{data:0})}}export{ye as component};
